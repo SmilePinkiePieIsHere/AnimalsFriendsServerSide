057ba7fc8997e4106b57f3f9d79cd708dcbd6578
SmilePinkiePieIsHere <pevanzi@abv.bg>
2021-03-21 13:20:36 +0200

    adding more endpoints

--- AnimalsFriends/Controllers/PostsController.cs
+++ AnimalsFriends/Controllers/PostsController.cs
@@ -1,15 +1,11 @@
 ﻿using AnimalsFriends.Helpers;
 using AnimalsFriends.Models;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace AnimalsFriends.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class PostsController : ControllerBase
     {
--- AnimalsFriends/Controllers/UsersController.cs
+++ AnimalsFriends/Controllers/UsersController.cs
@@ -1,15 +1,43 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AnimalsFriends.Helpers;
+using AnimalsFriends.Models;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace AnimalsFriends.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class UsersController : ControllerBase
     {
+        private readonly AnimalsFriendsContext _context;
+        public UsersController(AnimalsFriendsContext context)
+        {
+            _context = context;
+
+            _context.Database.EnsureCreated();
+        }
+
+        [HttpGet]
+        public IActionResult GetAll([FromQuery] QueryParameters queryParameters)
+        {
+            IQueryable<User> users = _context.Users;
+
+            users = users
+                .Skip(queryParameters.Size * (queryParameters.Page - 1))
+                .Take(queryParameters.Size);
+
+            return Ok(users.ToArray());
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetUser(int id)
+        {
+            var user = _context.Users.Find(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
     }
 }
